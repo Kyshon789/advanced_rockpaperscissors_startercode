acfd1144da7db7af27f4f63eb37a6a9d75332432
fightman <kyshon53@gmail.com>
2017-12-12 23:24:18 +0000

    rockpaperscissors

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
 //GLOBAL VARIABLES
-
-
-
+$("button").click(function(){
+var userchoice = $("input").val();
+alert(userchoice);
 
 // DOCUMENT READY FUNCTION BELOW
 
+});
